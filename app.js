28135c50fa38bbd3a94b7c0619a65f425bee0684
vegard-hd <vegard.hd@outlook.com>
2025-05-28 10:41:14 +0200

    testing husky; should fail

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.use("/tasks", tasksRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+  // next(createError(404));
 });
 
 // error handler
